8406d52791dd4ec543c9cf6ee3dbc85d4e045721
Mridul Gupta <emgi@live.in>
2019-06-18 03:48:56 +0530

    Corrected Artifacting Error and Spaceship

--- main.py
+++ main.py
@@ -23,16 +23,21 @@ def LoadShape(b, s, x, y):
 
 #Update Board
 def Update(b):
+    b2 = np.copy(b)
     nbrs=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
     for i in range(1,b.shape[0]-1):
         for j in range(1,b.shape[1]-1):
             s=0
             for k in nbrs:
                 s+=b[i+k[0],j+k[1]]
-            b[i,j] = (1 if (s==2 or s==3) else 0)
+            birth = (b[i,j]==0 and s==3)
+            alive = b[i,j]==1 and (s==2 or s==3)
+            b2[i,j] = 1 if (birth or alive) else 0
+    return b2
 
 #Load a shape
 LoadShape(Board, sSpaceShip, 10, 10)
+LoadShape(Board, sBlinker, 40, 40)
 
 #Display Image
 nFrameNo=0
@@ -45,7 +50,7 @@ plt.close()
 while(nFrameNo<nFrames):
     nFrameNo+=1
     print(nFrameNo)
-    Update(Board)
+    Board=Update(Board)
     plt.imshow(Board)
     plt.axis("off")
     plt.savefig("Images/{:06d}.png".format(nFrameNo), bbox_inches='tight')
